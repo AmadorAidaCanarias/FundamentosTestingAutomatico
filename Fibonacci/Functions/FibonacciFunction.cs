7719136c2f1c81acd75185ffdb58962c3993b56c
AmadorAidaCanarias <amlopez@aidacanarias.com>
2022-04-19 08:39:51 +0100

    Ejercicio Fibonacci Primer Commit

---
+++ Fibonacci/Functions/FibonacciFunction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciConsoleApp.Functions
+{
+    internal static class FibonacciFunction
+    {
+        internal static int Fibonacci(int input)
+        {
+            switch (input)
+            {
+                case 0: 
+                    return 0;
+                    break;
+                case 1: 
+                    return 1;
+                    break;
+                default:
+                    return Fibonacci(input - 1) + Fibonacci(input - 2);
+                    break;
+            }
+        }
+    }
+}
---
+++ Fibonacci/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FibonacciConsoleApp.Functions;
+
+Console.WriteLine($"Resultado para Fibonacci 0 deberiá ser 0 y es { FibonacciFunction.Fibonacci(0) }");
+Console.WriteLine($"Resultado para Fibonacci 1 debería ser 1 y es { FibonacciFunction.Fibonacci(1) }");
+Console.WriteLine($"Resultado para Fibonacci 2 debería ser 1 y es { FibonacciFunction.Fibonacci(2) }");
+Console.WriteLine($"Resultado para Fibonacci 3 debería ser 2 y es { FibonacciFunction.Fibonacci(3) }");
