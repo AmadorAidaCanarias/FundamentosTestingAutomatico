928070c4342828ef298c36bb49ff1b2d07abce3d
AmadorAidaCanarias <amlopez@aidacanarias.com>
2022-04-21 07:59:46 +0100

    Agregar clase para comprobar si es año bisiesto y su primer test para validar que cuando pasamos un año que sea divisible por 400 nos indique que sí es bisiesto.

---
+++ KatasAppTest/LeapYearTestClass.cs
@@ -0,0 +1,20 @@
+﻿using KatasApp.Services;
+using NUnit.Framework;
+
+
+namespace KatasTest
+{
+    public class LeapYearTestClass
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void WhenSendYearDivisibleBy400IsLeapYear()
+        {
+            Assert.AreEqual(true, LeapYear.LeapYearProcess(2400), "When pass 2400 Is Leap Year");
+        }
+    }
+}
