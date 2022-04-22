756109d7b826d334ff3fa0aca5dd1d6934d99dcd
AmadorAidaCanarias <amlopez@aidacanarias.com>
2022-04-22 07:30:08 +0100

    Pasar un año divisible por 100, pero no por 400 - Rojo

--- KatasAppTest/LeapYearTestClass.cs
+++ KatasAppTest/LeapYearTestClass.cs
@@ -14,8 +14,13 @@ namespace KatasTest.LeapYearTest
         [TestCase(true, 2400)]
         [TestCase(true, 2000)]
         [TestCase(true, 1600)]
-        [TestCase(false, 2300)]
         public void WhenSendYearDivisibleBy400IsLeapYear(bool expected, int yearToProcess)
+        {
+            Assert.AreEqual(expected, LeapYear.LeapYearProcess(yearToProcess), $"When pass { yearToProcess } Expected { expected }");
+        }
+
+        [TestCase(false, 2300)]
+        public void WhenSendYearDivisibleBy100ButNotBy400NotIsLeapYear(bool expected, int yearToProcess)
         {
             Assert.AreEqual(expected, LeapYear.LeapYearProcess(yearToProcess), $"When pass { yearToProcess } Expected { expected }");
         }
