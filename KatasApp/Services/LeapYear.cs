ab119bf772c05cf5de06d55e5202e2c043bce358
AmadorAidaCanarias <amlopez@aidacanarias.com>
2022-04-22 07:55:28 +0100

    Pasar a verde no divisible por 4 no es bisiesto

--- KatasApp/Services/LeapYear.cs
+++ KatasApp/Services/LeapYear.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace KatasApp.Services
+﻿namespace KatasApp.Services
 {
     public class LeapYear
     {
         public static bool LeapYearProcess(int yearToProcess)
         {
             bool isLeapYear = false;
-            if ((yearToProcess == 2001) || (yearToProcess == 2021) || (yearToProcess == 2011))
-                return true;
+            if (yearToProcess % 4 != 0)
+                return false;
             if (yearToProcess % 4 == 0)
                 isLeapYear = true;
             if ((yearToProcess % 100 == 0) && (yearToProcess % 400 != 0))
