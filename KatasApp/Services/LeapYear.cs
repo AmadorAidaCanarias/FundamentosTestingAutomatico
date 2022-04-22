004de5056111a8ce73795d4974042132bb4b6b6d
AmadorAidaCanarias <amlopez@aidacanarias.com>
2022-04-22 08:08:49 +0100

    Pasar a verde

--- KatasApp/Services/LeapYear.cs
+++ KatasApp/Services/LeapYear.cs
@@ -4,16 +4,7 @@
     {
         public static bool LeapYearProcess(int yearToProcess)
         {
-            bool isLeapYear = false;
-            if (yearToProcess % 4 != 0)
-                return false;
-            if (yearToProcess % 4 == 0)
-                isLeapYear = true;
-            if ((yearToProcess % 100 == 0) && (yearToProcess % 400 != 0))
-                isLeapYear = false;
-            if (yearToProcess % 400 == 0)
-                isLeapYear = true;
-            return isLeapYear;
+            return yearToProcess % 4 == 0;
         }
     }
 }
