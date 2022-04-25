108f864d9c2ad48c57cf66c5cd732f7d72ea9f16
AmadorAidaCanarias <amlopez@aidacanarias.com>
2022-04-25 11:58:31 +0100

    Pasar Empty String Test en Rojo

---
+++ KatasApp/Services/StringCalculator.cs
@@ -0,0 +1,12 @@
+﻿namespace KatasApp.Services
+{
+    public class StringCalculator
+    {
+        public static int Add(string numbers)
+        {
+            int result = -1;
+            
+            return result;
+        }
+    }
+}
---
+++ KatasAppTest/StringCalculatorTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KatasApp.Services;
+using FluentAssertions;
+
+namespace KatasTest.StringCalculatorTest
+{
+    public class StringCalculatorTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void WhenPassEmptyStringThenReturn0()
+        {
+            int resultStringCalculator = StringCalculator.Add("");
+
+            resultStringCalculator.Should().Be(0, "When Pass Empty String Then Return 0.");
+        }
+    }
+}
