d4f4ee972bbfab10a0da7bb272fe139b11d28917
AmadorAidaCanarias <amlopez@aidacanarias.com>
2022-05-09 21:19:46 +0100

    red: devolver 1 cuando se envía 0

---
+++ KatasApp/Services/Factorial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KatasApp.Services
+{
+    public class FactorialService
+    {
+        public int Factorial(int numberToCalculateFactorial)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ KatasAppTest/FactorialClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KatasApp.Services;
+using NUnit.Framework;
+
+namespace KatasTest
+{
+    public class FactorialClass
+    {
+        private FactorialService factorialService;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.factorialService = new FactorialService();
+        }
+
+        [Test]
+        public void should_return_1_when_factorial_is_0()
+        {
+            var result = this.factorialService.Factorial(0);
+            Assert.AreEqual(1, result);
+        }
+    }
+}
