a2d5fb50be07ad4f45e0bf6918a39505dd4dba92
AmadorAidaCanarias <amlopez@aidacanarias.com>
2022-05-10 07:47:01 +0100

    refactor: limpiar using

--- KatasApp/Services/Factorial.cs
+++ KatasApp/Services/Factorial.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace KatasApp.Services
+﻿namespace KatasApp.Services
 {
     public class FactorialService
     {
--- KatasAppTest/FactorialClass.cs
+++ KatasAppTest/FactorialClass.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using KatasApp.Services;
+﻿using KatasApp.Services;
 using NUnit.Framework;
+using System;
 
 namespace KatasTest
 {
