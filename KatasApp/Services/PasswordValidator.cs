14e533a75a7270e8a9f636a93ceb4d002f64b05b
AmadorAidaCanarias <amlopez@aidacanarias.com>
2022-05-24 19:37:08 +0100

    red: cuando enviamos un password con de menos de 8 caracteres debe devolver no valido y un mensaje que la contraseña debe tener al menos 8 caracteres

---
+++ KatasApp/Services/PasswordValidator.cs
@@ -0,0 +1,19 @@
+﻿namespace KatasApp.Services {
+    public class PasswordResultValidator {
+        public PasswordResultValidator()
+        {
+            IsValid = false;
+            Messages = new List<string>();
+        }
+
+        public bool IsValid { get; set; }
+        public List<string> Messages { get; set; }
+    }
+    
+    public class PasswordValidator {
+        public PasswordResultValidator Validate(string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ KatasAppTest/PasswordKataTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using FluentAssertions;
+using KatasApp.Services;
+using NUnit.Framework;
+
+namespace KatasTest.PasswordKataTest {
+    public class PasswordKataTest {
+
+        private PasswordValidator passwordValidator;
+
+        [SetUp]
+        public void Setup()
+        {
+            passwordValidator = new PasswordValidator();
+        }
+
+        [Test]
+        public void when_pass_password_with_length_minor_8_should_return_false_validation_and_message()
+        {
+            PasswordResultValidator resultValidator = passwordValidator.Validate("1234567");
+
+            resultValidator.IsValid.Should().BeFalse();
+            resultValidator.Messages.Count.Should().BeGreaterThanOrEqualTo(1);
+            resultValidator.Messages
+                .Any(x => x.Equals("La contraseña debe tener al menos 8 caracteres."))
+                .Should()
+                .BeTrue();
+        }
+
+    }
+}
