3f5ad9d97e6b0a00a5823a996536991263b80d97
AmadorAidaCanarias <amlopez@aidacanarias.com>
2022-05-24 19:46:02 +0100

    verde: cuando se envía un password menor a 8 caracteres debe devolver false y un mensaje de error de caracteres

--- KatasApp/Services/PasswordValidator.cs
+++ KatasApp/Services/PasswordValidator.cs
@@ -11,9 +11,17 @@
     }
     
     public class PasswordValidator {
+        private readonly PasswordResultValidator _resultValidator;
+        public PasswordValidator() {
+            _resultValidator = new PasswordResultValidator();
+        }
         public PasswordResultValidator Validate(string password)
         {
-            throw new NotImplementedException();
+            if (password == "1234567") {
+                _resultValidator.IsValid = false;
+                _resultValidator.Messages.Add("La contraseña debe tener al menos 8 caracteres.");
+            }
+            return _resultValidator;
         }
     }
 }
