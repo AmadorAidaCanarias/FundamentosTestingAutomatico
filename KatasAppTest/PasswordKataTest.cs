24f4125ffa2778d495c40ac314c983f96736c7b9
AmadorAidaCanarias <amlopez@aidacanarias.com>
2022-05-24 20:08:47 +0100

    rojo: cuando se envía una contraseña de una longitud menor a 8

--- KatasAppTest/PasswordKataTest.cs
+++ KatasAppTest/PasswordKataTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
 using KatasApp.Services;
 using NUnit.Framework;
@@ -14,15 +15,18 @@ namespace KatasTest.PasswordKataTest {
             passwordValidator = new PasswordValidator();
         }
 
-        [Test]
-        public void when_pass_password_with_length_minor_8_should_return_false_validation_and_message()
-        {
-            PasswordResultValidator resultValidator = passwordValidator.Validate("1234567");
 
-            resultValidator.IsValid.Should().BeFalse();
+        [Test]
+        [TestCase( "1234567", false, "La contraseña debe tener al menos 8 caracteres.")]
+        [TestCase("234567", false, "La contraseña debe tener al menos 8 caracteres.")]
+        public void when_pass_password_with_length_minor_8_should_return_false_validation_and_message(string password, bool expectedValid, string expectedMessage)
+        {
+            PasswordResultValidator resultValidator = passwordValidator.Validate(password);
+
+            resultValidator.IsValid.Should().Be(expectedValid);
             resultValidator.Messages.Count.Should().BeGreaterThanOrEqualTo(1);
             resultValidator.Messages
-                .Any(x => x.Equals("La contraseña debe tener al menos 8 caracteres."))
+                .Any(x => x.Equals(expectedMessage))
                 .Should()
                 .BeTrue();
         }
