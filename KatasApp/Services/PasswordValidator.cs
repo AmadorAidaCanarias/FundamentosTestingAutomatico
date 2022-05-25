d8d301f0c642015476ac6795630454ad5799fe17
AmadorAidaCanarias <amlopez@aidacanarias.com>
2022-05-25 08:54:07 +0100

    refactorizar: reducir duplicidad de valor a IsValid

--- KatasApp/Services/PasswordValidator.cs
+++ KatasApp/Services/PasswordValidator.cs
@@ -20,24 +20,22 @@ namespace KatasApp.Services {
         public PasswordResultValidator Validate(string password)
         {
             if (password.Length < 8) {
-                _resultValidator.IsValid = false;
                 _resultValidator.Messages.Add("La contraseña debe tener al menos 8 caracteres.");
             }
             var matchNegatives = Regex.Matches(password, @"\d");
             if (matchNegatives.Count < 2) {
-                _resultValidator.IsValid = false;
                 _resultValidator.Messages.Add("La contraseña debe tener al menos dos números.");
             }
             var matchUpper = Regex.Matches(password, @"[A-Z]");
             if (matchUpper.Count == 0) {
-                _resultValidator.IsValid = false;
                 _resultValidator.Messages.Add("La contraseña debe contener al menos una letra mayúscula.");
             }
             var matchSpecial = Regex.Matches(password, @"[^0-9a-zA-Z]+");
             if (matchSpecial.Count == 0) {
-                _resultValidator.IsValid = false;
                 _resultValidator.Messages.Add("La contraseña debe contener al menos un carácter especial.");
             }
+            _resultValidator.IsValid = (_resultValidator.Messages.Count == 0);
+
             return _resultValidator;
         }
     }
