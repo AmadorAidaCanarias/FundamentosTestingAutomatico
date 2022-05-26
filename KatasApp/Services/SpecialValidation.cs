51e9dff7b97c1769d1383b97b59ae687d9eea1af
AmadorAidaCanarias <amlopez@aidacanarias.com>
2022-05-26 08:19:24 +0100

    agregar validación especial

---
+++ KatasApp/Services/SpecialValidation.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace KatasApp.Services;
+
+public class SpecialValidation : Validation {
+    private readonly string message = "La contraseña debe contener al menos un carácter especial.";
+    public bool Validate(string input, ref List<string> messages) {
+        bool isValid = true;
+        if (Regex.Matches(input, @"[^0-9a-zA-Z]+").Count == 0) {
+            messages.Add("La contraseña debe contener al menos un carácter especial.");
+            isValid = false;
+        }
+
+        return isValid;
+    }
+}
