714f9b2f1084381c3ae3df0409519e848b7d4b89
AmadorAidaCanarias <amlopez@aidacanarias.com>
2022-05-26 08:11:06 +0100

    Crear implementación de Validation para la validación de los números

---
+++ KatasApp/Services/NumberValidation.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace KatasApp.Services;
+
+public class NumberValidation: Validation
+{
+    private readonly string message;
+    public NumberValidation()
+    {
+        message = "La contraseña debe tener al menos dos números.";
+    }
+    public bool Validate(string input, ref List<string> messages)
+    {
+        bool isValid = true;
+        if (Regex.Matches(input, @"\d").Count < 2)
+        {
+            isValid = false;
+            messages.Add(message);
+        }
+        return isValid;
+    }
+}
