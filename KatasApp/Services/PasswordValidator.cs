f7f29b14e8b5f8e9548932d597e89bd548dd2668
AmadorAidaCanarias <amlopez@aidacanarias.com>
2022-05-26 08:41:43 +0100

    eliminación de propiedades innecesarias

--- KatasApp/Services/PasswordValidator.cs
+++ KatasApp/Services/PasswordValidator.cs
@@ -2,21 +2,14 @@
 
 namespace KatasApp.Services {
     public class PasswordValidator {
-        private readonly PasswordResultValidator _resultValidator;
         private readonly List<Validation> validations;
         public PasswordValidator(List<Validation> validations) {
-            _resultValidator = new PasswordResultValidator();
             this.validations = validations;
         }
-        public PasswordResultValidator Validate(string password)
-        {
+        public PasswordResultValidator Validate(string password) {
             List<string> messages = new();
             validations.ForEach(validation => validation.Validate(password, ref messages));
-
-            _resultValidator.IsValid = (messages.Count == 0);
-            _resultValidator.Messages.AddRange(messages);
-
-            return _resultValidator;
+            return new PasswordResultValidator() { IsValid = (messages.Count == 0), Messages = messages };
         }
     }
 }
